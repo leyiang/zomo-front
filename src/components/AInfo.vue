2486b12b00298ed97de7765d7056591c348e460c
mr.doob <yiangforwork@gmail.com>
2021-12-09 18:17:47 +0800

    Component: Add AInfo Component clear

---
+++ src/components/AInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="info flex align-center">
+    <i :class="['iconfont', 'icon-' + icon ]"></i>
+    <span><slot></slot></span>
+  </span>
+</template>
+
+<script>
+  export default {
+    props: {
+      icon: {
+        type: String,
+        default: "good"
+      }
+    }
+  }
+</script>
