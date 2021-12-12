2e216a30f9e41710f63b1f5d12ed1a6068023f70
mr.doob <yiangforwork@gmail.com>
2021-12-12 18:31:50 +0800

    page-tarnsition middle

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app">
-    <router-view :key="$route.fullPath"/>
-  </div>
+    <div id="app">
+        <div class="page-transition flex flex-col">
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+        </div>
+
+        <router-view :key="$route.fullPath"/>
+    </div>
 </template>
