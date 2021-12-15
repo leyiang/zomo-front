30a1f0c9fa6e729a17e6747dbf80b17ea5661615
mr.doob <yiangforwork@gmail.com>
2021-12-15 10:54:06 +0800

    add swiperjs

--- src/components/ASlider.vue
+++ src/components/ASlider.vue
@@ -1,31 +1,50 @@
 <style>
-  .slider-wrapper {
+.my-slider {
     height: 450px;
     background-color: #dedeef;
     user-select: none;
-  }
+}
 
-  .slider-wrapper.with-margin {
+.my-slider.with-margin {
     margin-bottom: 40px;
-  }
+}
 
-  .slide img {
+.swiper-slide img {
     width: 100%;
     height: 100%;
     object-fit: cover;
     object-position: center;
-  }
+}
 </style>
 
 <template>
-  <div class="slider-wrapper radius">
-    <div class="slider-inner h-full">
-      <div class="slide h-full">
-        <img src="@/assets/slide-template.png" alt="slide png">
-      </div>
+    <div class="my-slider radius">
+        <div class="slider-inner swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="@/assets/slide-template.png" alt="png">
+            </div>
+
+            <div class="swiper-slide">
+                <img src="@/assets/slide-template.png" alt="png">
+            </div>
+
+            <div class="swiper-slide">
+                <img src="@/assets/slide-template.png" alt="png">
+            </div>
+
+            <div class="swiper-slide">
+                <img src="@/assets/slide-template.png" alt="png">
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
+import Swiper from "swiper";
+import "swiper/swiper-bundle.min.css"
+export default {
+    mounted() {
+        const swiper = new Swiper(".my-slider", {});
+    }
+}
 </script>
